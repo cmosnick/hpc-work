2cf471940f64180ebee0386ee27a70ce3e3a6b1d
Christina Mosnick <cmosnick07@gmail.com>
2016-02-24 21:55:58 -0600

    Added sloppy tests for function, re organized so tests could be better organized

---
+++ homework2/analyze/hw2_analyze.cpp
@@ -0,0 +1,79 @@
+// #include <iostream>
+// #include <cstdlib>
+// #include <algorithm>
+// #include <chrono>
+
+// #include <boost/numeric/ublas/io.hpp>
+
+#include "../src/main.hpp"
+
+int main(int argc, char * argv[])
+{
+	
+	// ---------------------------------------------
+	// BEGIN: Timing Analysis
+	// ---------------------------------------------
+	std::cout << "Running Timing Analysis" << std::endl
+		  << "-----------------------" << std::endl;
+	const unsigned int ITR= 100;
+	const unsigned int PROCS = 12;
+	const unsigned int RESULTS = 100;
+	
+	// ---------------------------------------------
+	// Read in file to test on
+	// ---------------------------------------------
+	map< string, vector<float> > files;
+	map< string, uint > fnames;
+	vector< pair< uint, vector<float> > > lines;
+
+
+// Change this to 1 to enable larger file
+#if 0
+	const char fname[16] = "../8400_HPC.csv";
+#else
+	const char fname[16] = "../8400_HPC.csv";
+#endif
+
+	FILE *infile = fopen(fname, "r");
+	if(!infile){
+		cout << "File invalid" << endl;
+		exit(0);
+	}
+	int numLines = read_in_file(&files, infile, fnames, lines);
+	std::cout << numLines << std::endl;
+	string queryFilename("agricultural/agricultural00.tif");
+	// ***********************************
+	// Test functor : Your implementation
+	// ***********************************
+
+	// Iterate through  1-12 processes
+	for (int i = 1 ; i <= PROCS ; i++)
+	{
+		// Run Timing Experiment
+                std::chrono::high_resolution_clock c;
+                std::chrono::high_resolution_clock::time_point start = c.now();
+		for (unsigned int i = 0; i < ITR; ++i)
+		{
+			// This is an assignemnt of a call to a object functor;
+			process_query(fnames, lines, queryFilename, RESULTS, i);
+		}
+                std::chrono::high_resolution_clock::time_point stop = c.now();
+		double avgMs = (double) std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / (1000000 * ITR);
+
+		// Compute Ops and Elements
+		// totally wring estimaytion of operations
+		// const unsigned long opsMaybe = numLines * NUM_FLOATS;
+		const unsigned long elements = numLines;
+		// Log timing statistics
+		std::cout << "------------------------------------------------------------------" << std::endl
+			  << ITR << " iterations of file querying ran using " << fname << std::endl
+			  << "Average Time (s): " << avgMs<< std::endl
+			  << "Data  Point:f:" << avgMs << ":" << i << ":" << elements << std::endl;
+	}
+	std::cout << "Timing Analysis Completed" << std::endl
+		  << "=========================" << std::endl;
+	// ---------------------------------------------
+	// END: Timing Analysis
+	// ---------------------------------------------
+}
+
---
+++ homework2/src/main.hpp
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <math.h>
+#include <sys/shm.h>
+
+using namespace std;
+
+#define NUM_FLOATS 4097
+#define FNAME_SIZE 256
+#define FLOAT_CHARS 47
+#define LINE_SIZE FNAME_SIZE + ( NUM_FLOATS * (FLOAT_CHARS + 3) )
+#define DELIMS ","
+
+typedef struct fnameToLineNum{
+	char fname[FNAME_SIZE];
+	uint lineNum;
+}fnameToLineNum_t;
+
+typedef struct lineNumToValues{
+	uint lineNum;
+	float values[NUM_FLOATS];
+}lineNumToValues_t;
+
+typedef struct lineDistance{
+	uint lineNum;
+	float distance;
+}lineDistance_t;
+
+typedef struct childInfo{
+	lineDistance_t *start;
+	lineDistance_t *end;
+	uint startLine;
+	uint endLine;
+	uint shmStartLine;
+	uint shmEndline;
+	// uint queryLine;	// Line of queried filename, so the result doent show up as 0
+}childInfo_t;
+
+
+int read_in_file(map< string, vector<float> > *files, FILE *infile, map<string, uint> &fnames, vector< pair< uint, vector<float> > > &lines);
+bool is_float(const char* token);
+void print_vector(std::vector<float> *vector);
+bool process_query(map<string, uint> &fnames, vector< pair< uint, vector<float> > > &lines, string queryFilename, int numResults, int numProcesses);
+float compute_L1_norm(const vector<float> *v1, const vector<float> *v2);
+bool do_work(int processNumber, childInfo_t childInfo, const vector<float> *targetVector, const vector<pair<uint, vector<float> > > &lines, int numResults);
+bool shm_setup(size_t size, lineDistance_t *shm);
+void print_lines(vector<pair<uint, vector<float> > > &lines);
+void print_filenames(map<string, uint> &fnames);
+bool isLine(const pair<uint, vector<float> > pair, uint lineNum);
+void print_line_distances(vector<pair<uint, float> > &lineDistances);
+void print_shm(lineDistance_t *start, const lineDistance_t *end);
+bool comp(pair<uint, float> &el1, pair<uint, float> &el2);
+string find_fname_by_linenum(map<string, uint> &fnames, uint lineNum);
+
+
+
+
+
+
+
