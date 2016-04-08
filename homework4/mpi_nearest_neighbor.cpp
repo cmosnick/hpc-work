adcec1a9d306a8e165c5ef5db5eff4fa48d790bd
Christina Mosnick <cmosnick07@gmail.com>
2016-04-07 22:48:08 -0500

    Successful read of directory- regular files only

--- homework4/mpi_nearest_neighbor.cpp
+++ homework4/mpi_nearest_neighbor.cpp
@@ -2,9 +2,11 @@
 #include "directory_scanner.hpp"
 
 typedef std::map<std::string,scottgs::path_list_type> content_type;
+typedef content_type::const_iterator content_type_citr;
 
 namespace cmoz{
     void parseFiles(const content_type directoryContents);
+    void printDirContents(const scottgs::path_list_type file_list);
 
 }
 
@@ -28,25 +30,22 @@ int main(int argc, char *argv[]){
         }
 
         // Read directory
-        
+        content_type directoryContents;
+        scottgs::path_list_type *fileList;
         try{
-            content_type directoryContents = scottgs::getFiles(argv[2]);
-
+            directoryContents = scottgs::getFiles(argv[2]);
+            const std::string regular_file("REGULAR");
+            fileList = &(directoryContents[regular_file]);
         }
         catch(std::exception& e){
             std::cout << "Something went wrong reading the input directory." << std::endl;
             MPI_Finalize();
             exit(0);
         }
+        std::cout << "Sucessful reading directory" << std::endl;
 
-        // Check input file (in place of directory read for now)
-        // FILE *infile = fopen(argv[2], "r");
-        // if(!infile){
-        //     std::cout << "File invalid" << std::endl;
-        //     MPI_Finalize();
-        //     exit(0);
-        // }
-
+        // cmoz::printDirContents(directoryContents);
+        cmoz::printDirContents(*fileList);
 
 
 
@@ -69,6 +68,29 @@ void cmoz::parseFiles(const content_type directoryContents){
 
 
  }
+
+
+void cmoz::printDirContents(const scottgs::path_list_type file_list){
+    // For each type of file found in the directory, 
+    // List all files of that type
+
+     // for (content_type_citr f = directoryContents.begin(); 
+     //     f!=directoryContents.end();
+     //     ++f)
+     // {
+         // const scottgs::path_list_type file_list(f->second);
+            
+         // std::cout << "Showing: " << f->first << " type files (" << file_list.size() << ")" << std::endl;
+         for (scottgs::path_list_type::const_iterator i = file_list.begin();
+             i!=file_list.end(); ++i)
+         {
+             //boost::filesystem::path file_path(boost::filesystem::system_complete(*i));
+             boost::filesystem::path file_path(*i);
+             std::cout << "\t" << file_path.string() << std::endl;
+         }
+                
+     // }
+}
 
 
 
