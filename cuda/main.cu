fe3a743e946792519d7b758ee3104298996a92c6
cmosnick <cmosnick07@gmail.com>
2016-04-27 16:28:00 +0000

    Got correctly formatted file now, can copy picture

--- cuda/main.cu
+++ cuda/main.cu
@@ -8,33 +8,66 @@
 #define DEBUG_MESSAGES_ON 1
 
 #define GRID_SIZE   
-#define BLOCK_SIZE  32
-
-
-// const char *imageFilename = "lena.ppm";
-// const char *refFilename   = "lena_blurred.ppm";
+#define BLOCK_SIZE  4
 
 
 // Texture reference for 2D float texture
-texture<unsigned char, 2, cudaReadModeElementType> tex;
+texture<float, 2, cudaReadModeElementType> tex;
 
 
 // Kernel function to perform blurring
 __global__ void blurKernel(float *outputData, int width, int height, int filterSize){
-    // calculate normalized texture coordinates
-    // unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
-    // unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+    extern __shared__ uint window[];
 
-    // float u = (float)x - (float)width/2; 
-    // float v = (float)y - (float)height/2; 
-    // float tu = u*cosf(theta) - v*sinf(theta); 
-    // float tv = v*cosf(theta) + u*sinf(theta); 
+    // calculate texture coordinates
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+    // unsigned int tid=threadIdx.y*blockDim.y+ threadIdx.x;
 
-    // tu /= (float)width; 
-    // tv /= (float)height; 
 
-    // // read from texture and write to global memory
-    // outputData[y*width + x] = tex2D(tex, tu+0.5f, tv+0.5f);
+    uint pixelRadius = filterSize >> 1;
+
+    if(x <= pixelRadius || y <= pixelRadius || x >= (width-pixelRadius) || y >= (height-pixelRadius)){
+        // Do nothing
+    }
+    else{
+        // uint x_start    = x - pixelRadius;
+        // uint x_end      = x + pixelRadius;
+        // uint y_start    = y - pixelRadius;
+        // uint y_end      = y + pixelRadius;
+
+        // int arraySize = filterSize * filterSize;
+        // // Create 2D to hold values in.  Each row is 
+
+        // // Fill array with values
+        // for(int i = x_start, ii=0 ; i <= x_end; i++, ii++){
+        //     for(int j =  y_start, jj=0 ; j <= y_end ; j++, jj++){
+        //         window[(tid * arraySize) + (ii * filterSize) + jj] = tex2D(tex, i, j);
+        //         // window[(tid * arraySize) + jj] = 0;
+        //     }
+        // }
+        // syncthreads();
+
+        // // Partial bubble sort from https://anisrahman.wordpress.com/2010/02/02/2d-median-filtering-using-cuda/
+        // int halfArraySize = (arraySize/2) + 1;
+        // for(int i = 0 ; i < halfArraySize ; i++){
+        //     int min = i;
+        //     for(int j = 0 ; j < arraySize ; j++){
+        //         if(window[(tid * arraySize) + (i * filterSize) + j] < window[(tid * arraySize) + (i * filterSize) + min]){
+        //             min = j;
+        //         }
+        //     }
+        //     // swap min into place
+        //     unsigned char temp = window[(tid * arraySize) + i];
+        //     window[(tid * arraySize) + i]     = window[(tid * arraySize) + min];
+        //     window[(tid * arraySize) + min]   = temp;
+        // }
+        // // get median
+        // outputData[(y * width) + x] = window[(tid * arraySize) + halfArraySize-1];
+
+        outputData[(y*width) + x] = tex2D(tex, x, y);
+        syncthreads();
+    }
 }
 
 
@@ -86,46 +119,46 @@ int main(int argc, char **argv){
     }
 
     sdkLoadPGM(imagePath, &origData, &width, &height);
+    char *outimagePath = sdkFindFilePath(outputfile, argv[0]);
+    if (outimagePath == NULL)
+    {
+        #if DEBUG_MESSAGES_ON
+        std::cout << "Unable to source image file:"<< outputfile << "\n" << std::endl;
+        #endif
+        exit(EXIT_FAILURE);
+    }
+    // Sanity check to make sure file is loaded in correctly
+    // TODO: either file is not loaded correctly, or it is not saving correctly
+    sdkSavePGM(outimagePath, origData, width, height);
 
-    unsigned char size = width * height * sizeof(unsigned char);
-    #if DEBUG_MESSAGES_ON
-    std::cout << "Loaded " << inputfile << ", " << width << " x "<< height << " pixels with size " << size << std::endl;
-    #endif
+    // sdkSavePGM(outputfile, &origData, &width, &height);
 
-    // //Load reference image from image (output)
-    // float *outData = (float *) malloc(size);
-    // char *outPath = sdkFindFilePath(outputfile, argv[0]);
+    // int size = width * height * sizeof(float);
+    // #if DEBUG_MESSAGES_ON
+    // std::cout << "Loaded " << inputfile << ", " << width << " x "<< height << " pixels with size " << (uint)size << std::endl;
+    // #endif
 
-    // if (outPath == NULL)
-    // {
-    //     std::cout << "Unable to find reference image file: " << outputfile << "\n" << std::endl;
-    //     exit(EXIT_FAILURE);
-    // }
+    // // Allocate device memory for result
+    // float *outData = NULL;
+    // checkCudaErrors(cudaMalloc((void **) &outData, size));
 
-    // sdkLoadPGM(outPath, &outData, &width, &height);
+    // // Allocate array and copy image data
+    // cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(8, 0, 0, 0, cudaChannelFormatKindUnsigned);
+    // cudaArray *inArray;
+    // checkCudaErrors(cudaMallocArray(&inArray,
+    //                                 &channelDesc,
+    //                                 width,
+    //                                 height));
+    // checkCudaErrors(cudaMemcpyToArray(inArray,
+    //                                   0,
+    //                                   0,
+    //                                   origData,
+    //                                   size,
+    //                                   cudaMemcpyHostToDevice));
 
-
-    // Allocate device memory for result
-    float *outData = NULL;
-    checkCudaErrors(cudaMalloc((void **) &outData, size));
-
-    // Allocate array and copy image data
-    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(8, 0, 0, 0, cudaChannelFormatKindUnsigned);
-    cudaArray *inArray;
-    checkCudaErrors(cudaMallocArray(&inArray,
-                                    &channelDesc,
-                                    width,
-                                    height));
-    checkCudaErrors(cudaMemcpyToArray(inArray,
-                                      0,
-                                      0,
-                                      origData,
-                                      size,
-                                      cudaMemcpyHostToDevice));
-
-    #if DEBUG_MESSAGES_ON
-    std::cout << "\n\nLoaded " << inputfile << " onto device." << std::endl;
-    #endif
+    // #if DEBUG_MESSAGES_ON
+    // std::cout << "\n\nLoaded " << inputfile << " onto device." << std::endl;
+    // #endif
 
     // Set texture parameters
     tex.addressMode[0] = cudaAddressModeBorder;
@@ -133,23 +166,45 @@ int main(int argc, char **argv){
     tex.filterMode = cudaFilterModePoint;
     tex.normalized = false;    // access with normalized texture coordinates
 
-    // Bind the array to the texture
-    checkCudaErrors(cudaBindTextureToArray(tex, inArray, channelDesc));
+    // // Bind the array to the texture
+    // checkCudaErrors(cudaBindTextureToArray(tex, inArray, channelDesc));
 
-    uint pixelRadius = filterSize >> 1;
-    #if DEBUG_MESSAGES_ON
-    std::cout << "Picxel radius is " << pixelRadius << std::endl;
-    #endif
-    // Set up grid
-    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
-    dim3 dimGrid(width / dimBlock.x, height / dimBlock.y, 1);
+    // uint pixelRadius = filterSize >> 1;
+    // #if DEBUG_MESSAGES_ON
+    // std::cout << "Pixel radius is " << pixelRadius << std::endl;
+    // #endif
+    // // Set up grid
+    // dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
+    // dim3 dimGrid(width / dimBlock.x, height / dimBlock.y, 1);
 
-    #if DEBUG_MESSAGES_ON
-    std::cout << "\n\nBlocjks and grid set up.\nBlock is " << dimBlock.x << " x " << dimBlock.y << \
-        "\nGrid is " << dimGrid.x << " x " << dimGrid.y << std::endl;
-    #endif
+    // #if DEBUG_MESSAGES_ON
+    // std::cout << "\n\nBlocks and grid set up.\nBlock is " << dimBlock.x << " x " << dimBlock.y << \
+    //     "\nGrid is " << dimGrid.x << " x " << dimGrid.y << std::endl;
+    // #endif
 
-    blurKernel<<<dimGrid, dimBlock, 0>>>(outData, width, height, filterSize);
+    // int window_size = BLOCK_SIZE * BLOCK_SIZE * filterSize * filterSize * sizeof(uint);
+    // getLastCudaError("Before Kernel execution");
+
+    // #if DEBUG_MESSAGES_ON
+    // std::cout << "Window size is: " << window_size << std::endl;
+    // #endif
+
+    // blurKernel<<<dimGrid, dimBlock, window_size>>>(outData, width, height, filterSize);
+    // getLastCudaError("Kernel execution failed");
+
+
+    // // Allocate mem for the result on host side
+    // unsigned char *hOutputData = (unsigned char *) malloc(size);
+    // // copy result from device to host
+    // checkCudaErrors(cudaMemcpy(hOutputData,
+    //                            outData,
+    //                            size,
+    //                            cudaMemcpyDeviceToHost));
+    // // Write to file
+    // // sdkSavePGM(outputfile, hOutputData, width, height);
+    // #if DEBUG_MESSAGES_ON
+    // std::cout << "Wrote to " << outputfile << "." << std::endl;
+    // #endif
 
 
 
